d95a0d3a86c3bd0ee3122dc03e0d41f1e719b025
Willian Galvani <williangalvani@gmail.com>
2022-05-17 14:25:12 -0300

    required trim stuff, needs testing

--- ArduSub/motors.cpp
+++ ArduSub/motors.cpp
@@ -18,7 +18,20 @@ void Sub::motors_output()
         verify_motor_test();
     } else {
         motors.set_interlock(true);
+        // output any servo channels
+        SRV_Channels::calc_pwm();
+
+        // cork now, so that all channel outputs happen at once
+        SRV_Channels::cork();
+
+        // update output on any aux channels, for manual passthru
+        SRV_Channels::output_ch_all();
+
+        // send output signals to motors
         motors.output();
+
+        // push all channels
+        SRV_Channels::push();
     }
 }
 
