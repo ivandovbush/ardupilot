031da5159bd8c6f2816e7c3d41c46719c40ecc0d
Willian Galvani <williangalvani@gmail.com>
2022-06-26 12:50:04 -0700

    unify scripts

--- Rover/scripts/powercontrol.lua
+++ Rover/scripts/powercontrol.lua
@@ -32,14 +32,110 @@ local POWER_SAFE = 30.0
 
 -- CREATE CUSTOM PARAMS
 local PARAM_TABLE_KEY = 72
-assert(param:add_table(PARAM_TABLE_KEY, "SOLAR_", 2), 'could not add param table')
+assert(param:add_table(PARAM_TABLE_KEY, "SOLAR_", 3), 'could not add param table')
 
 assert(param:add_param(PARAM_TABLE_KEY, 1, 'OVERRIDE', 0), 'could not add OVERRIDE PARAM')
 assert(param:add_param(PARAM_TABLE_KEY, 2, 'KP', 20), 'could not add KP PARAM')
+assert(param:add_param(PARAM_TABLE_KEY, 3, 'MAX_PERC', 30), 'could not add MA_PERC PARAM')
 
 local override = bind_param("SOLAR_OVERRIDE")
 local kp = bind_param("SOLAR_KP")
+local max_thrust_percentage = bind_param("SOLAR_MAX_PERC")
+
+-- start VERY slowly
+param:set('MOT_THR_MAX', 3)
+
+persistence_file = 'last_waypoint.txt'
+ARMING_SWITCH = 27
+DEFAULT_WAIT_TIME = 5*60 -- 5 minutes to arm and do its thing.
+
+INPUT = 0
+OUPUT = 1
+
+-- CREATE CUSTOM PARAMS
+local PARAM_TABLE_KEY = 75
+assert(param:add_table(PARAM_TABLE_KEY, "MISSION_", 1), 'could not add param table')
+assert(param:add_param(PARAM_TABLE_KEY, 1, 'PAUSE_S', DEFAULT_WAIT_TIME), 'could not add PAUSE param')
+
+
+gpio:pinMode(ARMING_SWITCH, INPUT)
+
+gcs:send_text(6, 'Started mission persistency layer')
+gcs:send_text(6, 'using file ' .. persistence_file)
+
 -----------------------------------------
+
+
+function reload_waypoint()
+    file = assert(io.open(persistence_file, "r"), 'Could open :' .. persistence_file)
+    if not file then -- file doesnt exist yet?
+      gcs:send_text(0, 'unable to open file, giving up')
+      return
+    end
+    io.input(file)
+    local waypoint_number = io.read("*number")
+    if waypoint_number then
+      mission:  (waypoint_number)
+      gcs:send_text(0, string.format("loaded waypoint: %.0f", waypoint_number))
+    else
+      gcs:send_text(0, 'unable to read waypoint, giving up')
+    end
+end
+
+
+function mission_control() -- this is the loop which periodically runs
+    local should_pause = param:get('MISSION_PAUSE_S')
+    param:set('MISSION_PAUSE_S', math.min(should_pause, 86400))-- do not allow it to go above 1 day
+    if should_pause > 0 then
+      param:set('MISSION_PAUSE_S', should_pause - 1) -- decrease 1 second at a time
+      gcs:send_text(6,string.format("waiting for more %.0f seconds", should_pause))
+      return update, 1000
+    end
+    if not gpio:read(27) then
+      if  arming:is_armed() then
+        gcs:send_text(0, 'Disarming via onboard switch')
+        arming:disarm()
+      end
+      return update, 1000
+    end
+    if gpio:read(27) and not arming:is_armed() and not state == STATE_CRITICAL  then
+      gcs:send_text(6,string.format("attempting to arm"))
+      arming:arm()
+      return update, 3000
+    end
+    if (vehicle:get_mode() == MODE_GUIDED) then
+      local curr_loc = ahrs:get_location()
+      local target_loc = vehicle:get_target_location()
+      if not target_loc then
+        gcs:send_text(6,string.format("bad target_loc!!!"))
+      end
+      if curr_loc and target_loc then
+        local dist_m = curr_loc:get_distance(target_loc)
+        gcs:send_text(6,string.format("distance: %.0f ", dist_m))
+        if (dist_m < 10.0) then
+          reload_waypoint()
+          vehicle:set_mode(MODE_AUTO)
+        end
+      end
+    end
+    if (vehicle:get_mode() ~= MODE_AUTO) then
+          gcs:send_text(6,string.format("setting to auto mode..."))
+          vehicle:set_mode(MODE_AUTO)
+          return update, 1000 -- reschedules the loop
+    end
+    local number = mission:get_current_nav_index()
+    local mission_state = mission:state()
+
+    if mission:get_current_nav_index() <= 1 then
+       gcs:send_text(6,string.format("attempting to load waypoint"))
+       reload_waypoint()
+    end
+    if (mission:get_current_nav_index() ~= 0) then
+        file = assert(io.open(persistence_file, "w"), 'Could open :' .. persistence_file)
+        file:write(tostring(mission:get_current_nav_index()))
+    end
+    return update, 10000
+  end
 
 -- Check if we lost voltage/current measurements
 function lost_voltage()
@@ -97,16 +193,12 @@ function power_control()
     local current_throttle_limit =  param:get('MOT_THR_MAX')
     local desired_power = param:get('BAT_DES_POWER_W')
     local new_current_throttle_limit = current_throttle_limit
-    local mode = vehicle:get_mode()
-    if not (mode == MODE_MANUAL or mode == MODE_AUTO or mode == MODE_MANUAL) then
-        return update, 1000
-    end
     if watts > desired_power + deadzone then
-        new_current_throttle_limit = math.max(current_throttle_limit - 1, 10)
+        new_current_throttle_limit = math.max(current_throttle_limit - 1, 0)
         param:set('MOT_THR_MAX', new_current_throttle_limit)
     end
     if watts < desired_power - deadzone then
-        new_current_throttle_limit = math.min(current_throttle_limit + 1, 100)
+        new_current_throttle_limit = math.min(current_throttle_limit + 1, max_thrust_percentage.get())  -- INCREASE BEFORE SENDING TO HAWAII
         param:set('MOT_THR_MAX', new_current_throttle_limit)
 
     end
@@ -126,6 +218,9 @@ function update() -- this is the loop which periodically runs
         update_standby()
     end
     power_control()
+    if state ~= STATE_CRITICAL then
+        mission_control()
+    end
     return update, 1000 -- reschedules the loop
 end
 
