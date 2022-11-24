74b04873f59a926779186178ec2b1382e318c1ab
Willian Galvani <williangalvani@gmail.com>
2022-11-24 10:55:27 -0300

    AP_HAL_Linux: fix hal declarations

--- libraries/AP_HAL_Linux/RCOutput_PCA9685.cpp
+++ libraries/AP_HAL_Linux/RCOutput_PCA9685.cpp
@@ -56,7 +56,7 @@ using namespace Linux;
 
 #define PWM_CHAN_COUNT 16
 
-static const AP_HAL::HAL& hal = AP_HAL::get_HAL();
+extern const AP_HAL::HAL& hal;
 
 RCOutput_PCA9685::RCOutput_PCA9685(AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev,
                                    uint32_t external_clock,
